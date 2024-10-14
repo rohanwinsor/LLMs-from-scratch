8805e8f8d8d298dcc34a28fc437f84da1c6633b9
rohanwinsor <rohan.w.charles@gmail.com>
2024-10-14 09:17:05 +0530

    deleted few files

--- download_gpt.py
+++
@@ -1,5 +0,0 @@
-from gpt_download import download_and_load_gpt2
-
-settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
-print("Settings:", settings)
-print("Parameter dictionary keys:", params.keys())
